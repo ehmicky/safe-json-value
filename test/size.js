ad5b9a5240da324ff3b2a52e7d47b8204fa44f44
ehmicky <ehmicky@gmail.com>
2022-07-24 15:39:59 +0200

    Add more tests

--- test/size.js
+++ test/size.js
@@ -182,19 +182,28 @@ each(
   },
 )
 
-test('Applies options.maxSize recursively', (t) => {
-  const input = { one: { two: { three: true, four: true } } }
-  const output = { one: { two: { three: true } } }
-  const maxSize = JSON.stringify(output).length
-  t.deepEqual(safeJsonValue(input, { maxSize }), {
-    value: output,
-    changes: [
-      {
-        path: ['one', 'two', 'four'],
-        oldValue: true,
-        newValue: undefined,
-        reason: 'maxSize',
-      },
-    ],
-  })
-})
+each(
+  [
+    {
+      input: { one: { two: { three: true, four: true } } },
+      output: { one: { two: { three: true } } },
+      path: ['one', 'two', 'four'],
+    },
+    {
+      input: { one: { four: true, two: { three: true, four: true } } },
+      output: { one: { four: true, two: { three: true } } },
+      path: ['one', 'two', 'four'],
+    },
+  ],
+  ({ title }, { input, output, path }) => {
+    test(`Applies options.maxSize in a depth-first manner | ${title}`, (t) => {
+      const maxSize = JSON.stringify(output).length
+      t.deepEqual(safeJsonValue(input, { maxSize }), {
+        value: output,
+        changes: [
+          { path, oldValue: true, newValue: undefined, reason: 'maxSize' },
+        ],
+      })
+    })
+  },
+)
