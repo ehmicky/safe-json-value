2914f980eb645fc8fae9853f3cbed623ecf771df
ehmicky <ehmicky@gmail.com>
2022-07-24 11:39:43 +0200

    Improve logic

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 /* eslint-disable max-lines */
 import isPlainObj from 'is-plain-obj'
 
+// Non-goals of this library:
+//  - Keeping or transtyping incompatible values
+//     - We only omit them, since this is simpler to handle for consumer
+//     - I.e. the result is lossy
+//  - Canonicalizing the value
+//  - Supporting other formats than JSON
 export default function safeJsonValue(
   value,
   // eslint-disable-next-line no-unused-vars
@@ -11,12 +17,28 @@ export default function safeJsonValue(
   return { value: valueA, changes }
 }
 
+// The final top-level return value:
+//  - Might be `undefined`
+//  - Is not serialized to a string, as this allows:
+//     - Choosing the serialization format and library
+//        - e.g. normal JSON, canonical JSON, YAML, etc.
+//     - Further processing on the value before serialization
+//     - Delaying the serialization
+//        - e.g. when `process.send()` is used with Node.js
 const transformValue = function (value, changes, path) {
   const valueA = callToJSON(value, changes, path)
   const valueB = filterValue(valueA, changes, path)
-  return recurseValue(valueB, changes, path)
+  addNotArrayIndexChanges(valueB, changes, path)
+  const valueC = recurseValue(valueB, changes, path)
+  return valueC
 }
 
+// Replace `object.toJSON()` by its return value.
+//  - Including for native classes like `Date`
+// This ensures this is resolved right now instead of during `JSON.stringify()`.
+// If the return value has `toJSON()` itself, it is ignored.
+//  - This is to mimic `JSON.stringify()` behavior.
+// If `object.toJSON()` throws, `object` is omitted.
 const callToJSON = function (value, changes, path) {
   if (!hasToJSON(value)) {
     return value
@@ -48,6 +70,10 @@ const hasToJSON = function (value) {
   )
 }
 
+// Omit types that are not supported by `JSON.stringify()`:
+//  - bigints would throw
+//  - `NaN` and `[-]Infinity` would be transformed to `null`
+//  - `undefined`, functions and symbols would be omitted
 const filterValue = function (value, changes, path) {
   if (!shouldFilter(value)) {
     return value
@@ -62,57 +88,24 @@ const filterValue = function (value, changes, path) {
 }
 
 const shouldFilter = function (value) {
-  const typeofValue = typeof value
+  const type = typeof value
   return (
-    FILTERED_TYPES.has(typeofValue) ||
-    (typeofValue === 'number' && !Number.isFinite(value))
+    FILTERED_TYPES.has(type) || (type === 'number' && !Number.isFinite(value))
   )
 }
 
 const FILTERED_TYPES = new Set(['function', 'symbol', 'undefined', 'bigint'])
 
-const recurseValue = function (value, changes, path) {
-  if (!isObject(value)) {
-    return value
+// Omit array properties that are not indices.
+//  - This mimics `JSON.stringify()` behavior
+//  - Regardless of whether they are symbols and|or non-enumerable
+const addNotArrayIndexChanges = function (array, changes, path) {
+  if (!Array.isArray(array)) {
+    return
   }
 
-  return Array.isArray(value)
-    ? recurseArray(value, changes, path)
-    : recurseObject(value, changes, path)
-}
+  const arrayProps = getArrayProps(array.length)
 
-const isObject = function (value) {
-  return typeof value === 'object' && value !== null
-}
-
-const recurseArray = function (array, changes, path) {
-  const newArray = []
-  const arrayProps = new Set(KNOWN_ARRAY_PROPS)
-
-  // eslint-disable-next-line fp/no-loops, fp/no-mutation, fp/no-let
-  for (let index = 0; index < array.length; index += 1) {
-    arrayProps.add(String(index))
-    const item = transformProp({ parent: array, key: index, changes, path })
-
-    // eslint-disable-next-line max-depth
-    if (item !== undefined) {
-      // eslint-disable-next-line fp/no-mutating-methods
-      newArray.push(item)
-    }
-  }
-
-  addNotArrayIndexChanges({ array, arrayProps, changes, path })
-  return newArray
-}
-
-const KNOWN_ARRAY_PROPS = ['length']
-
-const addNotArrayIndexChanges = function ({
-  array,
-  arrayProps,
-  changes,
-  path,
-}) {
   // eslint-disable-next-line fp/no-loops
   for (const key of Reflect.ownKeys(array)) {
     // eslint-disable-next-line max-depth
@@ -127,15 +120,82 @@ const addNotArrayIndexChanges = function ({
   }
 }
 
+// `Array.length` is omitted by `JSON.stringify()`. But since every array has
+// this property, we do not add it to `changes`.
+const getArrayProps = function (length) {
+  const indices = Array.from({ length }, getArrayIndex)
+  const arrayProps = new Set(indices)
+  arrayProps.add('length')
+  return arrayProps
+}
+
+const getArrayIndex = function (_, index) {
+  return String(index)
+}
+
+// If the array property is a getter or Proxy hook, it might throw.
 const safeGetArrayProp = function (array, key) {
   try {
     return array[key]
   } catch {}
 }
 
+// Recurse over plain objects and arrays.
+// We use a depth-first traversal.
+//  - I.e. parent, then children, then siblings
+//  - This works better with `maxSize`
+//     - This allows stopping logic when `maxSize` is reached, resulting in
+//       better performance
+//     - This favors removing fewer big fields instead of more small fields,
+//       resulting in fewer `changes`
+//     - This favors maximizing the number of fields within the allowed
+//       `maxSize`
+//  - This is easier to implement
+const recurseValue = function (value, changes, path) {
+  if (!isObject(value)) {
+    return value
+  }
+
+  return Array.isArray(value)
+    ? recurseArray(value, changes, path)
+    : recurseObject(value, changes, path)
+}
+
+const isObject = function (value) {
+  return typeof value === 'object' && value !== null
+}
+
+// Recurse over array items.
+// Only array indices are kept.
+//  - Even if either not enumerable or if inherited, to mimic `JSON.stringify()`
+//    behavior
+// Omitted items are filtered out.
+//  - Otherwise, `JSON.stringify()` would transform them to `null`
+const recurseArray = function (array, changes, path) {
+  const newArray = []
+
+  // eslint-disable-next-line fp/no-loops, fp/no-mutation, fp/no-let
+  for (let index = 0; index < array.length; index += 1) {
+    const item = transformProp({ parent: array, key: index, changes, path })
+
+    // eslint-disable-next-line max-depth
+    if (item !== undefined) {
+      // eslint-disable-next-line fp/no-mutating-methods
+      newArray.push(item)
+    }
+  }
+
+  return newArray
+}
+
+// Recurse over object properties.
+// Omitted properties are completely ignored (as opposed to have a key but an
+// `undefined` value).
+// We iterate in `Reflect.ownKeys()` order, not in sorted keys order.
+//  - This is faster
+//  - This preserves the object properties order
 const recurseObject = function (object, changes, path) {
-  const newObject =
-    Object.getPrototypeOf(object) === null ? Object.create(null) : {}
+  const newObject = getNewObject(object)
 
   // eslint-disable-next-line fp/no-loops
   for (const key of Reflect.ownKeys(object)) {
@@ -152,6 +212,16 @@ const recurseObject = function (object, changes, path) {
   return newObject
 }
 
+// When the object has a `null` prototype, we keep it.
+//  - This reduces the number of changes
+//  - Also, `JSON.stringify()` handles those
+const getNewObject = function (object) {
+  return Object.getPrototypeOf(object) === null ? Object.create(null) : {}
+}
+
+// Inherited properties are omitted.
+// Therefore, classes are converted to plain objects.
+//  - This mimics `JSON.stringify()` behavior
 const addClassChange = function ({ object, newObject, changes, path }) {
   if (!isPlainObj(object)) {
     changes.push({
@@ -163,6 +233,7 @@ const addClassChange = function ({ object, newObject, changes, path }) {
   }
 }
 
+// Recurse over an object property or array index
 const transformProp = function ({ parent, key, changes, path }) {
   const pathA = [...path, key]
   const prop = safeGetProp({ parent, key, changes, path: pathA })
@@ -171,6 +242,10 @@ const transformProp = function ({ parent, key, changes, path }) {
   return propB
 }
 
+// `parent[key]` might be a getter or proxy hook. This resolves it.
+// If it throws, the property is omitted.
+// It is not possible to detect that a proxy is being used, except when it
+// throws, so we cannot add this to the `changes`.
 const safeGetProp = function ({ parent, key, changes, path }) {
   try {
     return getProp({ parent, key, changes, path })
@@ -195,6 +270,7 @@ const getProp = function ({ parent, key, changes, path }) {
   return prop
 }
 
+// When `parent[key]` was a getter and|or setter
 const addGetterChange = function ({
   changes,
   path,
@@ -206,6 +282,7 @@ const addGetterChange = function ({
   }
 }
 
+// We convert non-writable|configurable properties to writable|configurable
 const addDescriptorChange = function ({
   changes,
   path,
@@ -231,6 +308,9 @@ const addDescriptorChange = function ({
   }
 }
 
+// Omit properties which are ignored by `JSON.stringify()`:
+//  - Symbol keys
+//  - Non-enumerable properties, except in arrays
 const filterKey = function ({ parent, key, prop, changes, path }) {
   if (typeof key === 'symbol') {
     changes.push({
