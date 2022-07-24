69a03adfb989caf637dd1f057dcc6da55cf49958
ehmicky <ehmicky@gmail.com>
2022-07-24 17:32:25 +0200

    Add more documentation on types

--- src/main.d.ts
+++ src/main.d.ts
@@ -3,11 +3,15 @@
  */
 export interface Options {
   /**
+   * Maximum `JSON.stringify(value).length`.
+   * Additional properties beyond the size limit are omitted.
    *
    * @default Number.POSITIVE_INFINITY (no maximum size)
    *
    * @example
    * ```js
+   * const input = { one: true, two: 'a'.repeat(1e6) }
+   * JSON.stringify(safeJsonValue(input, { maxSize: 1e5 }).value) // '{"one":true}"
    * ```
    */
   maxSize?: number
@@ -39,28 +43,86 @@ type ReasonWithoutError =
   | 'symbolValue'
   | 'toJSON'
   | 'undefined'
+
+/**
+ * Reason why a property was changed.
+ */
 export type Reason = ReasonWithError | ReasonWithoutError
 
 /**
- *
+ * Change applied to [`value`](#value).
+ * Each item is an individual change to a specific property.
+ * A given property might have multiple changes, listed in order.
  */
 export type Change<ReasonValue extends Reason = Reason> = {
+  /**
+   * Property path.
+   */
   path: PropertyKey[]
+
+  /**
+   * Property value before the change.
+   */
   oldValue: unknown
+
+  /**
+   * Property value after the change.
+   * `undefined` means the property was omitted.
+   */
   newValue: unknown
+
+  /**
+   * Reason for the change.
+   */
   reason: ReasonValue
-} & (ReasonValue extends ReasonWithError ? { error: Error } : {})
+} & (ReasonValue extends ReasonWithError
+  ? {
+      /**
+       * Error that triggered the change.
+       */
+      error: Error
+    }
+  : {})
 
 /**
+ * Makes `value` JSON-safe by:
+ *  - Omitting properties which would throw, change type unexpectedly or be
+ *    filtered with `JSON.stringify()`
+ *  - Resolving properties which would change value with `JSON.stringify()`
+ *
+ * This never throws.
  *
  * @example
  * ```js
+ * const input = { one: true }
+ * input.self = input
+ *
+ * JSON.stringify(input) // Throws due to cycle
+ * const { value, changes } = safeJsonValue(input)
+ * JSON.stringify(value) // '{"one":true}"
+ *
+ * console.log(changes) // List of changed properties
+ * // [
+ * //   {
+ * //     path: ['self'],
+ * //     oldValue: <ref *1> { one: true, self: [Circular *1] },
+ * //     newValue: undefined,
+ * //     reason: 'cycle'
+ * //   }
+ * // ]
  * ```
  */
 export default function safeJsonValue<T>(
   value: T,
   options?: Options,
 ): {
+  /**
+   * Copy of the input `value` after applying all the changes to make
+   * it JSON-safe.
+   *
+   * The top-level `value` itself might be changed (including to `undefined`) if
+   * it is either invalid JSON or has a `toJSON()` method.
+   */
   value: PartialDeep<T> | undefined
   changes: Change[]
 }
