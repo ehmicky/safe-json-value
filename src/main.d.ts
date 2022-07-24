0ef5fbed3c193ec9ddebea70bc2f6b8c25724784
ehmicky <ehmicky@gmail.com>
2022-07-24 15:48:00 +0200

    Improve types

--- src/main.d.ts
+++ src/main.d.ts
@@ -1,7 +1,24 @@
 /**
  * `safe-json-value` options
  */
-export interface Options {}
+export interface Options {
+  /**
+   *
+   * @default Number.POSITIVE_INFINITY
+   *
+   * @example
+   * ```js
+   * ```
+   */
+  maxSize?: number
+}
+
+// Make an object/array deeply optional.
+type PartialDeep<T> = T extends Array<infer ArrayItem>
+  ? Array<PartialDeep<ArrayItem>>
+  : T extends object
+  ? { [key in keyof T]?: PartialDeep<T[key]> }
+  : T
 
 /**
  *
@@ -9,4 +26,7 @@ export interface Options {}
  * ```js
  * ```
  */
-export default function safeJsonValue<T>(value: T, options?: Options): T
+export default function safeJsonValue<T>(
+  value: T,
+  options?: Options,
+): PartialDeep<T> | undefined
