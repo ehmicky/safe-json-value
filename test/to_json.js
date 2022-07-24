092d86ad78e7a860b69955634b4fccfccdf1ae3e
ehmicky <ehmicky@gmail.com>
2022-07-24 15:27:38 +0200

    Add more tests

--- test/to_json.js
+++ test/to_json.js
@@ -1,5 +1,6 @@
 import test from 'ava'
 import safeJsonValue from 'safe-json-value'
+import { each } from 'test-each'
 
 test('Call object.toJSON()', (t) => {
   const input = {
@@ -60,19 +61,42 @@ test('Handles object.toJSON() that throw', (t) => {
   ])
 })
 
-test('Handles object.toJSON() that call the library itself', (t) => {
-  const input = {
-    one: true,
-    two: undefined,
-    toJSON() {
-      // eslint-disable-next-line fp/no-this
+each(
+  [
+    function toJSON() {
+      // eslint-disable-next-line fp/no-this, no-invalid-this
       return safeJsonValue(this).value
     },
+    function toJSON() {
+      // eslint-disable-next-line fp/no-this, no-invalid-this
+      return safeJsonValue({ ...this }).value
+    },
+  ],
+  ({ title }, toJSON) => {
+    test(`Handles object.toJSON() that calls the library itself | ${title}`, (t) => {
+      const input = { one: true, two: undefined, toJSON }
+      const value = input.toJSON()
+      t.false('two' in value)
+      t.false('toJSON' in value)
+      t.deepEqual(value, { one: true })
+    })
+  },
+)
+
+test('Handles object.toJSON() that call the library itself with a parent', (t) => {
+  const input = {
+    prop: {
+      one: true,
+      two: undefined,
+      toJSON() {
+        return safeJsonValue(input).value
+      },
+    },
   }
-  const value = input.toJSON()
-  t.false('two' in value)
-  t.false('toJSON' in value)
-  t.deepEqual(value, { one: true })
+  const value = input.prop.toJSON()
+  t.false('two' in value.prop)
+  t.false('toJSON' in value.prop)
+  t.deepEqual(value, { prop: { prop: { one: true } } })
 })
 
 test('Handles object.toJSON that are not functions', (t) => {
