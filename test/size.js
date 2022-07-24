6bf2c0f0259aa706423a03c395eb0a48fde409ba
ehmicky <ehmicky@gmail.com>
2022-07-24 15:29:41 +0200

    Add more tests

--- test/size.js
+++ test/size.js
@@ -31,3 +31,23 @@ each(
     })
   },
 )
+
+each(
+  // eslint-disable-next-line unicorn/no-null, no-magic-numbers
+  [{}, [], true, false, null, 0, 0.1, -0, -1, 1e60, 1e-60, '', 'test'],
+  ({ title }, input) => {
+    test(`Computes size correctly | ${title}`, (t) => {
+      const size = JSON.stringify(input).length
+      t.deepEqual(safeJsonValue(input, { maxSize: size }), {
+        value: input,
+        changes: [],
+      })
+      t.deepEqual(safeJsonValue(input, { maxSize: size - 1 }), {
+        value: undefined,
+        changes: [
+          { path: [], oldValue: input, newValue: undefined, reason: 'maxSize' },
+        ],
+      })
+    })
+  },
+)
