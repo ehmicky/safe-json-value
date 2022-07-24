f00bc4ca25ca95fd8ebb1940ddaf43e69de5225c
ehmicky <ehmicky@gmail.com>
2022-07-24 15:46:21 +0200

    Add more tests

--- test/uncaught.js
+++ test/uncaught.js
@@ -1,23 +1,37 @@
 import test from 'ava'
 import safeJsonValue from 'safe-json-value'
+import { each } from 'test-each'
 
 const isProp = function (key) {
   return key === 'prop'
 }
 
-test('Handle dynamic infinite functions', (t) => {
-  const getInput = () => ({
-    // eslint-disable-next-line fp/no-get-set
-    get prop() {
-      return getInput()
-    },
-  })
-  const input = getInput()
-  const { value, changes } = safeJsonValue(input)
-  t.true('prop' in value)
-  const lastChange = changes[changes.length - 1]
-  t.true(Array.isArray(lastChange.path) && lastChange.path.every(isProp))
-  t.true('prop' in lastChange.oldValue)
-  t.is(lastChange.newValue, undefined)
-  t.is(lastChange.reason, 'uncaughtException')
+const getInfiniteGetter = () => ({
+  // eslint-disable-next-line fp/no-get-set
+  get prop() {
+    return getInfiniteGetter()
+  },
 })
+
+const getInfiniteToJSON = () => ({
+  prop: true,
+  toJSON() {
+    return { prop: getInfiniteToJSON() }
+  },
+})
+
+each(
+  [{ getInput: getInfiniteGetter }, { getInput: getInfiniteToJSON }],
+  ({ title }, { getInput }) => {
+    test(`Handle dynamic infinite functions | ${title}`, (t) => {
+      const input = getInput()
+      const { value, changes } = safeJsonValue(input)
+      t.true('prop' in value)
+      const lastChange = changes[changes.length - 1]
+      t.true(Array.isArray(lastChange.path) && lastChange.path.every(isProp))
+      t.is(typeof lastChange.oldValue, 'object')
+      t.is(lastChange.newValue, undefined)
+      t.is(lastChange.reason, 'uncaughtException')
+    })
+  },
+)
