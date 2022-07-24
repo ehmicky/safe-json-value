c537419f2893d1da286db414abcf5d09118e17d6
ehmicky <ehmicky@gmail.com>
2022-07-24 19:23:17 +0200

    Add test

---
+++ test/size/large.js
@@ -0,0 +1,34 @@
+import test from 'ava'
+import safeJsonValue from 'safe-json-value'
+
+const V8_MAX_STRING_LENGTH = 5e8
+const largeString = '\n'.repeat(V8_MAX_STRING_LENGTH)
+
+test('Handles very large strings', (t) => {
+  const maxSize = JSON.stringify({ one: '' }).length
+  t.deepEqual(safeJsonValue({ one: largeString }, { maxSize }), {
+    value: {},
+    changes: [
+      {
+        path: ['one'],
+        oldValue: largeString,
+        newValue: undefined,
+        reason: 'maxSize',
+      },
+    ],
+  })
+})
+
+test('Handles very large object properties', (t) => {
+  t.deepEqual(safeJsonValue({ [largeString]: true }, { maxSize: 2 }), {
+    value: {},
+    changes: [
+      {
+        path: [largeString],
+        oldValue: true,
+        newValue: undefined,
+        reason: 'maxSize',
+      },
+    ],
+  })
+})
