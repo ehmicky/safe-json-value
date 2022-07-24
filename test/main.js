ba99f81699c7dbcc3e1ff60aba35295ffc09249e
ehmicky <ehmicky@gmail.com>
2022-07-24 15:20:53 +0200

    Start adding tests

--- test/main.js
+++ test/main.js
@@ -1,6 +1,31 @@
 import test from 'ava'
 import safeJsonValue from 'safe-json-value'
+import { each } from 'test-each'
 
-test('Dummy test', (t) => {
-  t.is(typeof safeJsonValue, 'function')
-})
+const normalizeChanges = function (changes) {
+  return changes.map(normalizeChange)
+}
+
+const normalizeChange = function ([path, oldValue, newValue, reason]) {
+  return { path, oldValue, newValue, reason }
+}
+
+const noop = () => {}
+
+each(
+  [
+    {
+      input: noop,
+      value: undefined,
+      changes: [[[], noop, undefined, 'invalidType']],
+    },
+  ],
+  ({ title }, { input, value, changes }) => {
+    test(`Make JSON value safe | ${title}`, (t) => {
+      t.deepEqual(safeJsonValue(input), {
+        value,
+        changes: normalizeChanges(changes),
+      })
+    })
+  },
+)
