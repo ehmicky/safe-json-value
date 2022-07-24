70fd46e9fc11b3fc149b79ee5b6b9c2febd1a666
ehmicky <ehmicky@gmail.com>
2022-07-24 15:25:44 +0200

    Add more tests

---
+++ test/cycle.js
@@ -0,0 +1,13 @@
+import test from 'ava'
+import safeJsonValue from 'safe-json-value'
+
+test('Omit circular values', (t) => {
+  const input = {}
+  // eslint-disable-next-line fp/no-mutation
+  input.self = input
+  const { value, changes } = safeJsonValue(input)
+  t.false('self' in value)
+  t.deepEqual(changes, [
+    { path: ['self'], oldValue: input, newValue: undefined, reason: 'cycle' },
+  ])
+})
