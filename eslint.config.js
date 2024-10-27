bb9d90cc4620afdf8bd38adda833b50ec35499c2
ehmicky <ehmicky@gmail.com>
2024-10-27 14:21:02 +0000

    Fix linting

--- eslint.config.js
+++ eslint.config.js
@@ -1,1 +1,11 @@
-export { default } from '@ehmicky/eslint-config'
+import eslintConfig from '@ehmicky/eslint-config'
+
+export default [
+  ...eslintConfig,
+  {
+    rules: {
+      // This repository relies on JSON parsing/serializing
+      'unicorn/prefer-structured-clone': 0,
+    },
+  },
+]
